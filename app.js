7e2ed19717882be1d78ae81aff1987b361b6c67a
Ian Macalinao <me@ian.pw>
2014-12-21 16:48:45 -0600

    Add Procfile etc

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('client/'));
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('App listening on port ' + port);
+});
